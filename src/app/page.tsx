f051e258798728fad948b804a03b8b28f1578628
alexander <alexander@redrazr.com>
2025-02-11 14:41:56 +0300

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ import React, { useEffect } from "react";
 import { initMap } from "./helpers/map";
 
 import {guestsList} from './guests-list';
+import Image from "next/image";
+import HeroImage from '../../public/images/hero.jpg';
+import Arc from '../../public/icons/arc.gif';
+import Car from '../../public/icons/car.gif';
+import Cheers from '../../public/icons/cheers.gif';
+import Dance from '../../public/icons/dance.gif';
+import Dinner from '../../public/icons/dinner.gif';
+import Cake from '../../public/icons/cake.gif';
+import Finish from '../../public/icons/finish.gif';
 
 export default function Home() {
   useEffect(() => {
@@ -17,12 +26,9 @@ export default function Home() {
       <Script src={`https://api-maps.yandex.ru/v3/?apikey=${process.env.NEXT_PUBLIC_YANDEX_API_KEY}&lang=ru_RU`}
       strategy="beforeInteractive"
      />
-    {/* <div className={styles['language-selector']}>
-        <button >Eng</button>
-        <button >Ру</button>
-    </div> */}
     <div className={styles.hero}>
         <div className={styles['hero-content']}>
+        <Image src={HeroImage} alt="Aлександр & Виктория" priority fill/>
             <div className={styles.overlay}>
                 <h1 id="names">Александр & Виктория</h1>
                 <p id="wedding-date">13.09.2025</p>
@@ -35,20 +41,20 @@ export default function Home() {
         <p id="greetings">С огромным удовольствием приглашаем Вас на свадьбу</p>
     </section>
     <section className={styles['schedule-section']}>
-        <h2 id="schedule-title">Расписание</h2>
+        <h2 id="schedule-title">Свадебное расписание</h2>
         <ul className={styles['schedule-list']}>
-            <li><strong>15:30</strong> - Трансфер</li>
-            <li><strong>16:00</strong> - Сбор гостей/фуршет</li>
-            <li><strong>16:40</strong> - Церемония</li>
-            <li><strong>17:00</strong> - Праздничный ужин</li>
-            <li><strong>21:00</strong> - Торт</li>
-            <li><strong>22:00</strong> - Танцы</li>
-            <li><strong>23:00</strong> - Завершение вечера/трансфер</li>
+            <li><Image src={Car} alt="car" className={styles['icons']} unoptimized/><strong>15:30</strong>- Трансфер</li>
+            <li><Image src={Cheers} alt="cheers" className={styles['icons']} unoptimized/><strong>16:00</strong> - Сбор гостей/фуршет</li>
+            <li><Image src={Arc} alt="larch" className={styles['icons']} unoptimized/><strong>16:40</strong> - Церемония</li>
+            <li><Image src={Dinner} alt="dinner" className={styles['icons']}unoptimized /><strong>17:00</strong> - Праздничный ужин</li>
+            <li><Image src={Cake} alt="cake" className={styles['icons']} unoptimized/><strong>21:00</strong> - Торт</li>
+            <li><Image src={Dance} alt="dance" className={styles['icons']} unoptimized /><strong>22:00</strong> - Танцы</li>
+            <li><Image src={Finish} alt="end" className={styles['icons']} unoptimized/><strong>23:00</strong> - Завершение вечера</li>
         </ul>
     </section>
 
 <section className={styles['dressCode-section']}>
-    <h2 id="dressCode-title">dress code</h2>
+    <h2 id="dressCode-title">Dress code</h2>
     <p id="dressCode">
     Надевайте всё самое нарядное и красивое и обязательно берите с собой хорошее настроение!</p>
 </section>
@@ -63,43 +69,38 @@ export default function Home() {
     <section className={styles['details']}>
         <h2 id="details-title">Детали</h2>
         <ul className={styles['details-list']}>
-            <li>Если вы подготовили для нас сюрприз или творческий подарок, не забудьте предупредить нашy ведущyю. Она поможет воплотить вашу идею или отговорит вас, и ответит на все вопросы.</li>
-            <li>Не волнуйтесь, если вдруг не с кем оставить малыша. Мы очень рады видеть на своем празднике всю Вашу семью. На нашем празднике будет организован досуг для детей. Взрослые могут спокойно насладиться праздником.</li>
-            <li>не волнуйтесь о том, как потом добраться на наш праздник. Всех желающих будет ожидать трансфер в 15:30 который также в 00:00 совершит обратный маршрут в Санкт-Петербург</li>
+        <li>Как добраться на наш праздник? Всех желающих будет ожидать трансфер в 15:30, который также в 00:00 совершит обратный маршрут в Санкт-Петербург</li>
+        <li>Не волнуйтесь, если вдруг не с кем оставить малыша. Мы очень рады видеть на своем празднике всю Вашу семью. На нашем празднике будет организован досуг для детей. Взрослые могут спокойно насладиться праздником.</li>
+        <li>Если вы подготовили для нас сюрприз или творческий подарок, не забудьте предупредить нашy ведущyю. Она поможет воплотить вашу идею или отговорит вас, и ответит на все вопросы.</li>
         </ul>
     </section>
 
     <section className={styles['form-section']}>
-        <h2 id="form-title">Help us make your experience even better!</h2>
+        <h2 id="form-title">Ответьте на несколько вопросов</h2>
         <form className={styles['wedding-form']}>
-            <label id="transport-label">Do you need transport to the venue?</label>
+            <label id="transport-label">Потребуется ли вам трансфер?</label>
             <select id="transport" name="transport">
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
+                <option value="yes1">Только до торжества</option>
+                <option value="yes2">Только после торжества</option>
+                <option value="yes3">До и после торжества</option>
+                <option value="no">Не нужен</option>
             </select>
-
-            <label id="alcohol-label">Do you have any alcohol preferences?</label>
+            <label id="alcohol-label">Какой алкоголь предпочитаете?</label>
             <input type="text" id="alcohol" name="alcohol" placeholder="e.g. Red wine, beer" />
-
-            <label id="diet-label">Do you have any dietary restrictions?</label>
-            <input type="text" id="diet" name="diet" placeholder="e.g. Vegetarian, gluten-free" />
-
-            <label id="attendance-label">Will you attend the event?</label>
-            <select id="attendance" name="attendance">
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
-            </select>
-
+            <label id="diet-label">У вас есть какие-то ограничения в еде?</label>
+            <input type="text" id="diet" name="diet" placeholder="вегетарианец, алергия на мороженое" />
+            <label id="child-label">Будет ли с вами на празднике ребенок?</label>
+            <input type="text" id="child" name="child" placeholder="Будет ли с вами ребенок?" />
             <button type="submit" className={styles['submit-btn']} id="submit-btn">Отправить</button>
         </form>
     </section>
-
-    <div id="map" />
 
     <section className={styles['buttons']}>
         {/* <a href="#" className={styles['calendar-btn']} id="calendar-btn">Добавить в календарь</a> */}
         <a href="#" className={styles['accept-btn']} id="accept-btn">Принять приглашение</a>
     </section>
+
+    <div id="map" />
 
     <footer className={styles['footer']}>
         <p id="footer-text">&copy; 2025 Wedding of Alexandr & Viktoria. All rights reserved.</p>
