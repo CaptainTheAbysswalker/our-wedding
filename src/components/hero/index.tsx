fac99334b6960007752e2da52be2e586f495fa81
alexander <alexander@redrazr.com>
2025-02-20 18:42:49 +0300

    hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 
 import styles from "./hero.module.css";
 
-import HeroImage from '../../../public/images/hero.jpg';
+import HeroImage from '../../../public/images/hero11.jpg';
+import { TextEffect } from "../ui/text-effect";
 
 export const Hero = ()=>{
     return (
@@ -11,11 +12,11 @@ export const Hero = ()=>{
         <div className={styles["hero-content"]}>
           <Image src={HeroImage} alt="Aлександр & Виктория" priority />
           <div className={styles.overlay}>
-            <h1 id="names">Александр & Виктория</h1>
-            <p>13.09.2025</p>
-            <p id="quote" className={styles.quote}>
-              Together forever
-            </p>
+            <h1 id="names"><TextEffect preset='fade-in-blur' speedReveal={1.1} speedSegment={0.3} delay={3}> Александр & Виктория </TextEffect></h1>
+            <TextEffect className={styles.overlayText} speedReveal={1.1} speedSegment={0.3} delay={4}>13.09.2025</TextEffect>
+            {/* <p id="quote" className={styles.quote}>
+              
+            </p> */}
           </div>
         </div>
       </div>)
