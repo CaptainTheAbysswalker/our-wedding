e99413a4e31a5a1c600ee65596d7106d12bfbcd2
Alexander Severiukhin <alexander@redrazr.com>
2025-02-28 23:05:14 +0300

    add padding, line height, typo fix, calendar link

--- src/components/greetings/index.tsx
+++ src/components/greetings/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { TextAnimate } from "../magicui/text-animate";
+import { CALENDAR_URL } from "@/lib/constants";
 
 export const Greetings = ({guestName}: {guestName: string}) => {
+  const addToCalendarHandler = () => {
+    window?.open(CALENDAR_URL, "_blank")?.focus();
+  }
     return (
     <section>
       <h2 id="greetings-title">{guestName}!</h2>
@@ -9,6 +13,6 @@ export const Greetings = ({guestName}: {guestName: string}) => {
         С огромным удовольствием приглашаем Вас на нашу свадьбу, которая
         состоится
       </TextAnimate>
-      <p className="wedding-date">13 сентября 2025 года</p>
+      <p onClick={addToCalendarHandler} className="wedding-date">13 сентября 2025 года</p>
     </section>)
 }
